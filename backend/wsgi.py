098c958605f80316f8c798dadbbe4ee7c1cd0c6e
mojomartini <94409200+mojomartini@users.noreply.github.com>
2025-04-30 15:43:49 -0500

    Update wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,1 +1,8 @@
+import sys
+import pathlib
+# Ensure the backend folder is on the path
+project_root = pathlib.Path(__file__).resolve().parent
+sys.path.append(str(project_root))
+
+from backend.app.main import app as application  # Gunicorn looks for "application"
 
