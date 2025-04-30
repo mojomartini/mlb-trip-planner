f18831438c8d19bedee217ea7f5a21b37fedc636
mojomartini <94409200+mojomartini@users.noreply.github.com>
2025-04-30 15:43:12 -0500

    Update models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,1 +1,13 @@
+from typing import Optional
+from datetime import datetime
+from sqlmodel import SQLModel, Field
+
+class Game(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    date_utc: datetime
+    home_team: str
+    away_team: str
+    stadium: str
+    tz: str
+    first_pitch_local: datetime
 
